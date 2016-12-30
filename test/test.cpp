53ddbaebdd1f79fc82ef251fe9a4a2ffad412ca6
Aaryaman Sagar <rmn100@gmail.com>
2016-12-30 17:22:00 +0530

    Added lz4

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@
 #include <double-conversion/double-conversion.h>
 #include <jemalloc/jemalloc.h>
 #include <snappy.h>
+#include <lz4.h>
 
 #include <iostream>
 #include <string>
@@ -17,6 +18,8 @@ TEST(all, all) {
     auto to_compress = string{"aaaaaaaa"};
     auto compressed = to_compress;
     snappy::Compress(to_compress.data(), to_compress.size(), &compressed);
+    LZ4_compress_default(to_compress.data(), (char*)compressed.data(),
+            to_compress.size(), compressed.size());
 
     void* memory = malloc(1000);
     EXPECT_TRUE(static_cast<bool>(memory));
