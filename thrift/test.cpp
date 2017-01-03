af867d3eac3f88eea1b3dd30934c566e14e465d3
Aaryaman Sagar <rmn100@gmail.com>
2017-01-03 11:47:39 +0530

    Added test for thrift and added thrift SSL sources

---
+++ thrift/test.cpp
@@ -0,0 +1,10 @@
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/server/TThreadedServer.h>
+#include <thrift/server/TNonblockingServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/concurrency/ThreadManager.h>
+#include <gtest/gtest.h>
+
+TEST(thrift, thrift) {}
