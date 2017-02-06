0da98781aa7fb801adc4ef07fa8cdaf4d655c2b9
Aaryaman Sagar <rmn100@gmail.com>
2017-02-06 17:17:03 -0500

    [gtest] Added comments to test.cpp

--- gtest/test.cpp
+++ gtest/test.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+/**
+ * The fact that this macro is there and the file compiles and runs fine is a
+ * decent test for presence but just including another thing to be sure
+ */
 TEST(test, test) {
     // cast the function pointer to force a resolution to the right function
     // since it's an overloaded function
