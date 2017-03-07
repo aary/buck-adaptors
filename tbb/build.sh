4efb9edaa462c77549417f2d99f12543294fb922
Aaryaman Sagar <rmn100@gmail.com>
2017-03-07 00:21:28 -0500

    [tbb] changed build script to work

--- tbb/build.sh
+++ tbb/build.sh
@@ -1,5 +1,6 @@
 curl -OL https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb2017_20170118oss_src.tgz
-mv tbb2017_20170118oss_src.tgz tbb
+tar -xzvf tbb2017_20170118oss_src.tgz
+mv tbb2017_20170118oss tbb
 rm tbb2017_20170118oss_src.tgz
 cd tbb
 make extra_inc=big_iron.inc tbb
